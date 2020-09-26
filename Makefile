6fe5770eb025e076310975e78ccb127175e12845
Jeremy Qian <jq357@hotmail.com>
2020-09-25 20:43:08 -0400

    text output

--- Makefile
+++ Makefile
@@ -1,21 +1,23 @@
 CC=gcc
-CFLAGS= -Wall -g -O3 -fPIC
-CFLAGS_EXE= -Wall -g -O3 -lm
-OMP_FALGS= -fopenmp -lgomp
+CFLAGS= -Wall -g -O3 -fPIC -shared -std=c99
+CFLAGS_EXE= -Wall -g -O3 -lm -std=c99
+OMP_FALGS= -fopenmp -lgomp -lomp
 OBJ = sieve.o
+TARGET=main
+TARGET_LIB=libsieve.so
 
 %.o: %.c
 	$(CC) -c -o $@ $< $(CFLAGS) $(OMP_FALGS)
 
-sieve.so: $(OBJ)
-	$(LINK.c) -shared $^ -o $@
+$(TARGET_LIB): $(OBJ)
+	$(LINK.c) $(CFLAGS) $(OMP_FALGS) $^ -o $@
 
-sieve: $(OBJ) main.o
+$(TARGET): $(OBJ) main.o
 	$(CC) -o $@ $^ $(CFLAGS_EXE) $(OMP_FALGS)
 
-all: sieve sieve.so
+all: $(TARGET) $(TARGET_LIB)
 
 clean:
-	rm *.o *.so sieve
+	rm -rf *.o *.so sieve
 
 .PHONY: all clean
--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 from sieve import *
-from ctypes import *
+from ctypes import CDLL, c_ulong
+from os import getcwd
+import time
 
+testLimits = [1000, 10000, 100000, 1000000, 10000000, 30000000, 50000000, 70000000,100000000, 300000000,500000000,1000000000, 2000000000]
 
-testLimits = [10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000]
+soPath = getcwd() + "/libsieve.so"
+sieveC = CDLL(soPath)
+# Warmup
+sieveC.sieve(10)
 
-sieveC = ctype.CDLL("sieve.so")
-
-sieveC.sieve.argtypes = [ctypes.c_unsigned_long]
-
-ret = sieveC.sieve(1000000000)
-
-print(ret)
+for item in testLimits:
+	t0 = time.time()
+	sieveC.sieve(item)
+	t1 = time.time()
+	totalC = t1-t0
+	t0 = time.time()
+	sieve(item)
+	t1 = time.time()
+	totalP = t1-t0
+	print("C time: %f, Python time: %f, for primes from 0 to %d (Speedup = %f)" % (totalC, totalP, item, totalP/totalC))
